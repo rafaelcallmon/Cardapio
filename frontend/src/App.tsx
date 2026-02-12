b8960c4faaf4cab8d5e287787827fe28889b7289
Rafael Calmon Gonzaga Santos <rafacalmon27@gmail.com>
2026-02-12 16:59:59 -0300

    teste

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className='container'>
-      <h1>Cardápio</h1>
+      <h1>Cardápiooo</h1>
       <div className="card-grid">
         {data?.map(foodData => 
           <Card 
